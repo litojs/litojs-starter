3ca3b7e003d2ffa185f314dee0377fcd96ebd21a
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-11-17 11:44:37 +0700

    fix

--- apps/core/view.tsx
+++ apps/core/view.tsx
@@ -1,14 +1,18 @@
 export function Home({ name }: { name: string }) {
   return (
-    <main class="h-screen flex justify-center items-center">
-      <div class="w-[500px] border p-12 rounded-lg space-y-4">
+    <main class="h-screen flex justify-center items-center bg-neutral-900 text-white">
+      <div class="w-[500px] bg-gradient-to-b from-neutral-900 to-neutral-800/15 shadow-lg shadow-black/5 border border-neutral-800 p-12 rounded-lg space-y-12">
         <section>
-          <h1 class="text-4xl font-semibold">{name} framework</h1>
-          <p>A pragmatic javascript fullstack framework</p>
+          <h1 class="text-2xl font-semibold tracking-tight">
+            {name} framework
+          </h1>
+          <p class="text-neutral-500">
+            A pragmatic javascript fullstack framework
+          </p>
         </section>
         <a
           href="https://lito.indrazm.com"
-          class="block w-fit text-sm bg-indigo-600 text-white font-medium px-3 py-2 rounded-lg"
+          class="block w-fit text-sm bg-indigo-600 hover:bg-indigo-500 text-white font-normal px-3 py-2 rounded-lg transition duration-300"
         >
           Read Documentation
         </a>
