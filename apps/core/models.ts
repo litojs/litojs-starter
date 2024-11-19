7a1456a583b88407716fa8a89bc6a1571e20d25e
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-11-19 22:46:28 +0700

    install deps

--- apps/core/models.ts
+++ apps/core/models.ts
@@ -1,6 +1,8 @@
 import { Models } from "lito-orm/src/models";
 
 export const note = new Models("note", (col) => ({
-  id: col.integer().primaryKey().default(Date.now()),
+  id: col.text().primaryKey().default("cuid"),
   content: col.text().notNull(),
+  isDone: col.boolean().default(false),
+  createdAt: col.text().default("now"),
 }));
